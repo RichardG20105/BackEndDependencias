c25cf5d77e7377bcb0030e9259346cbab83aa5f8
Richard Gualancañay <richard20105@hotmail.com>
2022-06-02 17:02:27 -0500

    Aumento del tiempo del JSON Token

--- src/main/java/com/app/Dependencias/Seguridad/JwtTokenUtil.java
+++ src/main/java/com/app/Dependencias/Seguridad/JwtTokenUtil.java
@@ -18,7 +18,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 public class JwtTokenUtil implements Serializable{
 	private static final long serialVersionUID = -2550185165626007488L;
 
-	public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+	public static final long JWT_TOKEN_VALIDITY = 1L * 30 * 24 * 60 * 60;
 
 	@Value("${jwt.secret}")
 	private String secreto;
